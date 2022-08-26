af74a84a688feae63b052e88ceb8cf53a0f8f5a3
Pete Wall <pete@petewall.net>
2022-08-26 10:38:27 -0500

    Add version command
    Signed-off-by: Pete Wall <pete@petewall.net>

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # pylint: disable=no-value-for-parameter
 
+from os import read
+import readline
 import click
 from PIL import Image
 from configuration import Configuration
 from screen import Screen
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def version():
+    with open('./version', 'r') as version_file:
+        print(version_file.readline().strip())
+
+@cli.command()
 @click.option("--config", type=click.File('r'), required=True, help="Config file for the screen")
 @click.argument("image", required=True)
 def display(config, image):
@@ -16,4 +27,4 @@ def display(config, image):
     screen.set_image(Image.open(image))
 
 if __name__ == '__main__':
-    display()
+    cli()
