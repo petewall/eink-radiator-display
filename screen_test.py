78403c5f2be3c388f8da836fe85302808402a09e
Pete Wall <pete@petewall.net>
2022-10-05 12:02:55 -0500

    Need to fix the tests since Screen makes the palette now

--- screen_test.py
+++ screen_test.py
@@ -4,12 +4,11 @@ from hamcrest import assert_that, is_
 
 from PIL import Image
 
-from palette import make_palette
 from screen import Screen
 
 class ScreenTest(unittest.TestCase):
     def test_set_color_palette_quantizes_wrb(self):
-        screen = Screen(400, 300, make_palette(['white', 'red', 'black']))
+        screen = Screen(400, 300, ['white', 'red', 'black'])
 
         with Image.open('test/pencils.jpg') as image:
             quantized = screen.set_color_palette(image)
@@ -18,7 +17,7 @@ class ScreenTest(unittest.TestCase):
                 assert_that(quantized, is_(the_same_image_as(expected)))
 
     def test_set_color_palette_quantizes_wrbgb(self):
-        screen = Screen(400, 300, make_palette(['white', 'red', 'black', 'green', 'blue']))
+        screen = Screen(400, 300, ['white', 'red', 'black', 'green', 'blue'])
 
         with Image.open('test/pencils.jpg') as image:
             quantized = screen.set_color_palette(image)
@@ -27,7 +26,7 @@ class ScreenTest(unittest.TestCase):
                 assert_that(quantized, is_(the_same_image_as(expected)))
 
     def test_set_color_palette_flattens_transparent_images(self):
-        screen = Screen(400, 300, make_palette(['white', 'red', 'black', 'green', 'blue']))
+        screen = Screen(400, 300, ['white', 'red', 'black', 'green', 'blue'])
 
         with Image.open('test/mango.png') as image:
             quantized = screen.set_color_palette(image)
